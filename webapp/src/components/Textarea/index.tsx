54321359e73059852dc21793f7a7d29e265318ef
Dimpin <wdm.dimpin@gmail.com>
2025-01-27 08:26:26 +0500

    Lesson 46 done

--- webapp/src/components/Textarea/index.tsx
+++ webapp/src/components/Textarea/index.tsx
@@ -1,15 +1,33 @@
 import { type FormikProps } from 'formik'
+import css from './index.module.scss'
+import cn from 'classnames'
 
-export const Textarea = ({ name, label, formik }: { name: string; label: string; formik: FormikProps<any> }) => {
+export const Textarea = ({
+  name,
+  label,
+  formik,
+}: {
+  name: string
+  label: string
+  formik: FormikProps<any>
+  maxWidth?: number
+}) => {
   const value = formik.values[name]
   const error = formik.errors[name] as string | undefined
   const touched = formik.touched[name]
+  const disabled = formik.isSubmitting
+  const invalid = !!touched && !!error
 
   return (
-    <div style={{ marginBottom: 10 }}>
-      <label htmlFor={name}>{label}</label>
-      <br />
+    <div className={cn({ [css.field]: true, [css.disabled]: disabled })}>
+      <label className={css.label} htmlFor={name}>
+        {label}
+      </label>
       <textarea
+        className={cn({
+          [css.input]: true,
+          [css.invalid]: invalid,
+        })}
         onChange={(e) => {
           void formik.setFieldValue(name, e.target.value)
         }}
@@ -21,7 +39,7 @@ export const Textarea = ({ name, label, formik }: { name: string; label: string;
         id={name}
         disabled={formik.isSubmitting}
       />
-      {!!touched && !!error && <div style={{ color: 'red' }}>{error}</div>}
+      {invalid && <div className={css.error}>{error}</div>}
     </div>
   )
 }
