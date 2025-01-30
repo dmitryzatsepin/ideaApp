8e0c84588e99b0aef857df3aa02ee4a8f8b81369
Dimpin <wdm.dimpin@gmail.com>
2025-01-30 08:50:57 +0500

    Lesson 52 done

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,17 +1,26 @@
 import cors from 'cors'
 import express from 'express'
 import { applyTrpcToExpressApp } from './lib/trpc'
+import { type AppContext, createAppContext } from './lib/ctx'
 import { trpcRouter } from './router'
 
-const expressApp = express()
-expressApp.use(cors())
-expressApp.get('/ping', (req, res) => {
-  res.send('pong')
-})
+void (async () => {
+  let ctx: AppContext | null = null
+  try {
+    ctx = createAppContext()
+    const expressApp = express()
+    expressApp.use(cors())
+    expressApp.get('/ping', (req, res) => {
+      res.send('pong')
+    })
 
-applyTrpcToExpressApp(expressApp, trpcRouter)
-
-const PORT = 3000
-expressApp.listen(PORT, () => {
-  console.info(`Server is running on http://localhost:${PORT}`)
-})
+    applyTrpcToExpressApp(expressApp, ctx, trpcRouter)
+    const PORT = 3000
+    expressApp.listen(PORT, () => {
+      console.info(`Server is running on http://localhost:${PORT}`)
+    })
+  } catch (error) {
+    console.error(error)
+    await ctx?.stop()
+  }
+})()
