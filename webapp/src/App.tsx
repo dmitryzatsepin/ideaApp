1a964071d2763dfe88491cb036e9e9c0f864982d
Dimpin <wdm.dimpin@gmail.com>
2025-02-03 08:15:16 +0500

    Lusson 60 done

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -6,6 +6,7 @@ import { AllIdeasPage } from './pages/AllIdeasPage'
 import { ViewIdeaPage } from './pages/ViewIdeaPage'
 import { NewIdeaPage } from './pages/NewIdeaPage'
 import './styles/global.scss'
+import { SignUpPage } from './pages/SignUpPage'
 
 export const App = () => {
   return (
@@ -13,6 +14,7 @@ export const App = () => {
       <BrowserRouter>
         <Routes>
           <Route element={<Layout />}>
+            <Route path={routes.getSignUpRoute()} element={<SignUpPage />} />
             <Route path={routes.getAllIdeasRoute()} element={<AllIdeasPage />} />
             <Route path={routes.getNewIdeaRoute()} element={<NewIdeaPage />} />
             <Route path={routes.getViewIdeaRoute(routes.viewIdeaRouteParams)} element={<ViewIdeaPage />} />
