2139a32c46b35c746967da49705d162e76a70f0b
Dimpin <wdm.dimpin@gmail.com>
2025-03-04 09:23:40 +0500

    Lesson 80 done

--- webapp/src/components/Layout/index.tsx
+++ webapp/src/components/Layout/index.tsx
@@ -1,8 +1,10 @@
+import { createRef } from 'react'
 import { Link, Outlet } from 'react-router-dom'
 import { useMe } from '../../lib/ctx'
 import { getAllIdeasRoute, getNewIdeaRoute, getSignOutRoute, getSignUpRoute, getSignInRoute } from '../../lib/routes'
 import css from './index.module.scss'
 
+export const layoutContentElRef = createRef<HTMLDivElement>()
 export const Layout = () => {
   const me = useMe()
   return (
@@ -49,7 +51,7 @@ export const Layout = () => {
           )}
         </ul>
       </div>
-      <div className={css.content}>
+      <div className={css.content} ref={layoutContentElRef}>
         <Outlet />
       </div>
     </div>
